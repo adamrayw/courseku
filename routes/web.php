31f54512892f034326e992db9d0764b3199bdf57
adamray <adamrayw@users.noreply.github.com>
2021-08-27 18:04:53 +0700

    routes

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\CoursesController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TutorialsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('/home');
+});
+
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::get('/profile', [ProfileController::class, 'index']);
+Route::get('/learn/{course:slug}', [CoursesController::class, 'show']);
+
+Route::get('/course/{slug}', [TutorialsController::class, 'show']);
+
+Route::post('/course/{slug}/comment', [CommentController::class, 'store']);
+
+Route::post('/course/{slug}/like', [CommentController::class, 'storevote']);
+Route::post('/course/{slug}/dislike', [CommentController::class, 'removevote']);
+
+// Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth');
+
+Route::get('/{category:slug}', function (Category $category) {
+    return view('pages.field', [
+        'name_category' => $category->name,
+        'courses' => $category->course,
+        'title' => $category->name,
+    ]);
+});
