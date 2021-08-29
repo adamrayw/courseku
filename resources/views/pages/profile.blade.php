167d9cea7a03e692c91e7279ce90111e5c64d43b
adamray <adamrayw@users.noreply.github.com>
2021-08-29 12:57:31 +0700

    Update Profile UI

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Profile')
 
 @section('content')
-<section class="mt-20 w-full mx-4 md:w-5/12 md:mx-auto p-4 shadow bg-gray-100 rounded-md">
+<section class="mt-20 w-full mx-4 md:w-5/12 md:mx-auto">
     <div class="flex items-start">
         <div class="bg-gray-500 w-24 h-24 rounded-md p-4 flex justify-center items-center mr-4">
             <h1 class="font-bold mb-1 text-white text-4xl">{{ Auth()->user()->name[0] }}</h1>
@@ -11,12 +11,18 @@
         <div class="">
             <h3 class="text-gray-600 text-2xl font-semibold">{{ Auth()->user()->name }}</h3>
             <!-- <p class="text-gray-500 text-md font-semibold">{{-- '@'.Auth()->user()->username --}}</p> -->
-            <a href="" class="block mt-2 text-center border-2 border-blue-500 rounded-xl px-6 py-2">Follow</a>
+            <a href="" class="block mt-2 text-center border-2 text-gray-500 text-sm border-gray-500 rounded-xl px-4 py-2">Follow</a>
         </div>
     </div>
-    <div class="menu my-6">
-        <a href="#">Liked t</a>
-        <a href="#">Saved Post</a>
+    <div class="menu mt-6 text-center flex border shadow-md bg-gray-100 rounded-md">
+        <a href="#" class="w-full p-4 text-gray-600 ">Liked Tutorials</a>
+        <a href="#" class="w-full p-4 text-gray-600 ">Submitted</a>
+        <a href="#" class="w-full p-4 text-gray-600  ">Saved Tutorials</a>
+    </div>
+    <div class="bg-white p-4">
+        <div class="card">
+            <h4 class="text-gray-600">Javascript Dasar</h4>
+        </div>
     </div>
 </section>
 @endsection
