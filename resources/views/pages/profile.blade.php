a181c936f9773c78579227d763b956834505d734
adamray <adamrayw@users.noreply.github.com>
2021-08-30 23:38:03 +0700

    Add Tabs

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Profile')
 
 @section('content')
-<section class="mt-20 w-full mx-4 md:w-5/12 md:mx-auto">
-    <div class="flex justify-between items-start">
+<section class="mt-20 max-w-3xl mx-4 md:mx-auto">
+    <div class="flex flex-col md:flex-row justify-between items-start">
         <div class="flex items-start">
             <div class="bg-gray-500 w-24 h-24 p-4 flex justify-center items-center mr-4 rounded-md">
                 <h1 class="font-bold mb-1 text-white text-4xl">{{ Auth()->user()->name[0] }}</h1>
@@ -28,15 +28,32 @@
             <!-- <p class="text-gray-500 text-md font-semibold">{{-- '@'.Auth()->user()->username --}}</p> -->
         </div>
     </div>
-    <div class="menu mt-6 text-center flex border shadow-sm bg-gray-100 overflow-auto">
-        <a href="#" class="w-full p-4 text-gray-600 ">Liked Tutorials</a>
-        <a href="#" class="w-full p-4 text-gray-600 ">Submitted</a>
-        <a href="#" class="w-full p-4 text-gray-600  ">Saved Tutorials</a>
-    </div>
-    <div class="bg-white p-4">
-        <div class="card">
-            <h4 class="text-gray-600">Javascript Dasar</h4>
+    <div class="mt-4 " x-data="{active: 0}">
+        <div class="flex overflow-hidden">
+            <button class="px-4  py-2 w-full" x-on:click.prevent="active = 0" x-bind:class="{'bg-gray-600 rounded-md text-white': active === 0}">Liked</button>
+            <button class="px-4 py-2 w-full" x-on:click.prevent="active = 1" x-bind:class="{'bg-gray-600 rounded-md text-white': active === 1}">Saved</button>
+            <button class="px-4 py-2 w-full" x-on:click.prevent="active = 2" x-bind:class="{'bg-gray-600 rounded-md text-white': active === 2}">Submitted</button>
+        </div>
+        <div class="bg-black bg-opacity-10">
+            <div class="p-4 bg-white space-y-2" x-show.transition.in="active === 0">
+                <div class="">
+                    @foreach ($datas as $data)
+                    <div class="card">
+                        <h4 class="text-gray-600">{{ $data->tutorial->name }}</h4>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="p-4 space-y-2" x-show.transition.in="active === 1">
+                <h2 class="text-2xl">Panel 2 Using x-show.transition</h2>
+                <p>Panel 2 content</p>
+            </div>
+            <div class="p-4 space-y-2" x-show.transition.in="active === 2">
+                <h2 class="text-2xl">Panel 2 Using x-show.transition</h2>
+                <p>Panel 2 content</p>
+            </div>
         </div>
     </div>
+
 </section>
 @endsection
