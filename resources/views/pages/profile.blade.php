c345a2a2bf746b1b31a41478feb2ee378ad10caa
adamray <adamrayw@users.noreply.github.com>
2021-08-30 22:07:23 +0700

    Fix Profile UI

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -4,17 +4,31 @@
 
 @section('content')
 <section class="mt-20 w-full mx-4 md:w-5/12 md:mx-auto">
-    <div class="flex items-start">
-        <div class="bg-gray-500 w-24 h-24 rounded-md p-4 flex justify-center items-center mr-4">
-            <h1 class="font-bold mb-1 text-white text-4xl">{{ Auth()->user()->name[0] }}</h1>
+    <div class="flex justify-between items-start">
+        <div class="flex items-start">
+            <div class="bg-gray-500 w-24 h-24 p-4 flex justify-center items-center mr-4 rounded-md">
+                <h1 class="font-bold mb-1 text-white text-4xl">{{ Auth()->user()->name[0] }}</h1>
+            </div>
+            <div>
+                <h3 class="text-gray-600 text-2xl font-semibold">{{ Auth()->user()->name }}</h3>
+                <p class="text-gray-500">Member Since : {{ Auth()->user()->created_at->toFormattedDateString() }}</p>
+            </div>
         </div>
         <div class="">
-            <h3 class="text-gray-600 text-2xl font-semibold">{{ Auth()->user()->name }}</h3>
+            <div>
+                <div class="flex items-center">
+                    <img src="img/point.png" alt="point" width="18">
+                    <p class="ml-1 font-medium text-gray-600">Points</p>
+                </div>
+                <div class="mt-1">
+                    <p class="text-gray-500">2000</p>
+                </div>
+                <!-- <a href="#!" class="mt-2 inline-block text-center border-2 text-gray-500 text-sm border-gray-500 rounded-md px-6 py-2">Follow</a> -->
+            </div>
             <!-- <p class="text-gray-500 text-md font-semibold">{{-- '@'.Auth()->user()->username --}}</p> -->
-            <a href="" class="block mt-2 text-center border-2 text-gray-500 text-sm border-gray-500 rounded-xl px-4 py-2">Follow</a>
         </div>
     </div>
-    <div class="menu mt-6 text-center flex border shadow-md bg-gray-100 rounded-md">
+    <div class="menu mt-6 text-center flex border shadow-sm bg-gray-100 overflow-auto">
         <a href="#" class="w-full p-4 text-gray-600 ">Liked Tutorials</a>
         <a href="#" class="w-full p-4 text-gray-600 ">Submitted</a>
         <a href="#" class="w-full p-4 text-gray-600  ">Saved Tutorials</a>
