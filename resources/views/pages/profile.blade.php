f8d80eb3ca4c9388c9b5ec7915764242324899e9
adamray <adamrayw@users.noreply.github.com>
2021-08-31 15:49:42 +0700

    Update UI Profile page

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -36,22 +36,20 @@
         </div>
         <div class=" bg-opacity-10">
             <div class="p-4 bg-white space-y-2" x-show.transition.in="active === 0">
-                <div class="">
-                    @if (count($datas) > 0)
-                    @foreach ($datas as $data)
-                    <div class="card">
-                        @if ($data->tutorial)
-                        <h4 class="text-gray-600">{{ $data->tutorial->name }}</h4>
-                        @endif
-                    </div>
-                    @endforeach
-                    @else
-                    <div class="flex flex-col justify-center items-center">
-                        <img src="img/like.png" alt="like" class="block">
-                        <h1 class="mt-2 text-gray-600">No tutorials liked.</h1>
-                    </div>
+                @if (count($datas) > 0)
+                @foreach ($datas as $data)
+                <div class="card">
+                    @if ($data->tutorial)
+                    <h4 class="text-gray-600">{{ $data->tutorial->name }}</h4>
                     @endif
                 </div>
+                @endforeach
+                @else
+                <div class="flex flex-col justify-center items-center">
+                    <img src="img/like.png" alt="like" class="block">
+                    <h1 class="mt-2 text-gray-600">No tutorials liked.</h1>
+                </div>
+                @endif
             </div>
             <div class="p-4 bg-white space-y-2" x-show.transition.in="active === 1">
                 <div class="flex flex-col justify-center items-center">
