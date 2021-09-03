8625d3924968d7313d57cbc5655eb01e68a78040
adamray <adamrayw@users.noreply.github.com>
2021-09-04 00:55:39 +0700

    Remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,8 @@ Route::get('/course/{slug}', [TutorialsController::class, 'show']);
 
 Route::post('/course/{slug}/comment', [CommentController::class, 'store']);
 
-Route::post('/course/{slug}/like', [CommentController::class, 'storevote']);
-Route::post('/course/{slug}/dislike', [CommentController::class, 'removevote']);
+// Route::post('/course/{slug}/like', [CommentController::class, 'storevote']);
+// Route::post('/course/{slug}/dislike', [CommentController::class, 'removevote']);
 
 // Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth');
 
