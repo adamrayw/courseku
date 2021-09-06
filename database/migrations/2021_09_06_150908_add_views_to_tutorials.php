4a2be21d51272acaef79a02069833c940148b721
adamray <adamrayw@users.noreply.github.com>
2021-09-06 23:29:35 +0700

    Edit typedata migration for page count view

--- database/migrations/2021_09_06_150908_add_views_to_tutorials.php
+++ database/migrations/2021_09_06_150908_add_views_to_tutorials.php
@@ -14,7 +14,7 @@ class AddViewsToTutorials extends Migration
     public function up()
     {
         Schema::table('tutorials', function (Blueprint $table) {
-            $table->integer('views')->after('submitted_by')->nullable();
+            $table->integer('views')->after('submitted_by');
         });
     }
 
