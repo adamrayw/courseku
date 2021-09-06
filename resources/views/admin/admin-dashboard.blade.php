ecaa70e193bbef088166bfc7507525b48119e418
adamray <adamrayw@users.noreply.github.com>
2021-09-06 21:45:30 +0700

    Update UI for admin dashboard

--- resources/views/admin/admin-dashboard.blade.php
+++ resources/views/admin/admin-dashboard.blade.php
@@ -3,7 +3,32 @@
 @section('admin-title', 'Admin Dashboard')
 
 
-<section class="max-w-7xl mx-auto mt-20">
-    <h3 class="text-2xl font-semibold">Welcome, {{ Auth()->user()->name }}</h3>
+<section class="mt-20 px-4 md:px-12 w-full">
+    <div class="mb-6">
+        <h3 class="text-2xl md:text-4xl font-semibold text-gray-600">Hi, {{ Auth()->user()->name }}</h3>
+        <hr class="mt-2">
+    </div>
+    <div class="total-user grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div class="card bg-white rounded-md py-4 px-6 shadow text-xl text-center">
+            <p class="text-gray-600 font-medium text-2xl mb-2">10</p>
+            <p class="text-blue-600 font-bold">Users</p>
+        </div>
+        <div class="card bg-white rounded-md py-4 px-6 shadow text-xl text-center">
+            <p class="text-gray-600 font-medium text-2xl mb-2">10</p>
+            <p class="text-blue-600 font-bold">Tutorials</p>
+        </div>
+        <div class="card bg-white rounded-md py-4 px-6 shadow text-xl text-center">
+            <p class="text-gray-600 font-medium text-2xl mb-2">10</p>
+            <p class="text-blue-600 font-bold">Courses</p>
+        </div>
+    </div>
+    <div class="mt-4 grid md:grid-cols-3 gap-4">
+        <div class="card md:col-span-2 bg-white rounded-md shadow p-4 text-gray-600">
+            <h2>Tutorials active</h2>
+        </div>
+        <div class="card bg-white rounded-md shadow p-4 text-gray-600">
+            <h2>New User</h2>
+        </div>
+    </div>
 </section>
 @endsection
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -6,8 +6,9 @@
 <div class="my-20 mx-2 flex flex-col justify-center items-center">
     <div class="bg-white w-full md:w-1/4 shadow rounded-md p-5 mx-2">
         <div class="text-center">
-            <h1 class="text-4xl font-bold mb-2 text-blue-600">Welcome Admin!</h1>
-            <p class="text-sm text-gray-500">Login to dashboard</p>
+            <i class="fas fa-user-shield fa-4x mb-2 text-blue-600"></i>
+            <h1 class="text-4xl font-bold mb-2 text-blue-600">Admin Login</h1>
+            <!-- <p class="text-sm text-gray-500">Login to dashboard</p> -->
         </div>
         <hr class="mt-2 mb-2">
         @if (session('status'))
@@ -31,7 +32,7 @@
             <!-- <div class="mt-6">
                 <a href="/forgot-password" class="font-medium text-sm text-gray-500 float-right">Forgot Password?</a>
             </div> -->
-            <button type="submit" class="mt-6 text-center w-full bg-blue-700 rounded-md text-white py-3 font-medium hover:bg-blue-600 transition">Login</button>
+            <button type="submit" class="mt-6 text-center w-full bg-blue-700 rounded-md text-white py-3 font-medium hover:bg-blue-600 transition"><i class="fas fa-sign-in-alt mr-1"></i>Login</button>
             <!-- <p class="text-sm text-center my-4 text-gray-500">Don't have an account ? <a href="/register" class="underline">Sign Up</a></p> -->
         </form>
     </div>
