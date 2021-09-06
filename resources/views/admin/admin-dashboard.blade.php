ea3b0306e9434d640ce65bb31e6aa318cc8af657
adamray <adamrayw@users.noreply.github.com>
2021-09-06 23:02:24 +0700

    Add section  new user and tutorials active

--- resources/views/admin/admin-dashboard.blade.php
+++ resources/views/admin/admin-dashboard.blade.php
@@ -3,6 +3,8 @@
 @section('admin-title', 'Admin Dashboard')
 
 
+
+
 <section class="mt-20 px-4 md:px-12 w-full">
     <div class="mb-6">
         <h3 class="text-2xl md:text-4xl font-semibold text-gray-600">Hi, {{ Auth()->user()->name }}</h3>
@@ -10,24 +12,66 @@
     </div>
     <div class="total-user grid grid-cols-1 md:grid-cols-3 gap-4">
         <div class="card bg-white rounded-md py-4 px-6 shadow text-xl text-center">
-            <p class="text-gray-600 font-medium text-2xl mb-2">10</p>
+            <p class="text-gray-600 font-medium text-2xl mb-2">{{ count($users) }}</p>
             <p class="text-blue-600 font-bold">Users</p>
         </div>
+
         <div class="card bg-white rounded-md py-4 px-6 shadow text-xl text-center">
-            <p class="text-gray-600 font-medium text-2xl mb-2">10</p>
+            <p class="text-gray-600 font-medium text-2xl mb-2">{{ count($tutorials) }}</p>
             <p class="text-blue-600 font-bold">Tutorials</p>
         </div>
         <div class="card bg-white rounded-md py-4 px-6 shadow text-xl text-center">
-            <p class="text-gray-600 font-medium text-2xl mb-2">10</p>
+            <p class="text-gray-600 font-medium text-2xl mb-2">{{ count($courses) }}</p>
             <p class="text-blue-600 font-bold">Courses</p>
         </div>
     </div>
     <div class="mt-4 grid md:grid-cols-3 gap-4">
         <div class="card md:col-span-2 bg-white rounded-md shadow p-4 text-gray-600">
-            <h2>Tutorials active</h2>
+            <h2 class="font-bold mb-2 text-xl">Tutorials active</h2>
+            <div class="overflow-scroll h-48 w-full">
+                <table class="table-auto border-collapse border border-gray-400 relative">
+                    <thead>
+                        <tr class="text-left sticky top-0 bg-white">
+                            <th class="w-1/2 border border-gray-400 p-2">Title</th>
+                            <th class="w-1/4 border border-gray-400 p-2">Author</th>
+                            <th class="w-1/4 border border-gray-400 p-2">Views</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($tutorials as $tutorial)
+                        <tr>
+
+                            <td class="border border-gray-400 p-2"><a href="/course/{{ $tutorial->slug }}">{{ $tutorial->name }}</a></td>
+                            <td class="border border-gray-400 p-2">{{ $tutorial->author }}</td>
+                            <td class="border border-gray-400 p-2">{{ $tutorial->views }}</td>
+
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
         <div class="card bg-white rounded-md shadow p-4 text-gray-600">
-            <h2>New User</h2>
+            <h2 class="font-bold text-xl">5 New User</h2>
+            <hr class="my-2">
+            <table class="table-auto w-full">
+                <thead>
+                    <tr class="text-left">
+                        <th class="pb-2">No</th>
+                        <th class="pb-2">Name</th>
+                        <th class="pb-2"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                    <tr>
+                        <td class="pb-2">{{ $loop->iteration }}.</td>
+                        <td class="pb-2">{{ $user->name }}</td>
+                        <td class="pb-2 text-right text-sm text-gray-400">{{ $user->created_at->diffForHumans() }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </section>
