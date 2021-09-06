eec0a94c79a40a605061a0e94ae71a24b0b3e7a9
adamray <adamrayw@users.noreply.github.com>
2021-09-06 23:54:50 +0700

    Add migration for page count view

--- database/migrations/2021_09_06_164745_add_views_to_tutorials.php
+++ database/migrations/2021_09_06_164745_add_views_to_tutorials.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DropViewsToTutorials extends Migration
+class AddViewsToTutorials extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class DropViewsToTutorials extends Migration
     public function up()
     {
         Schema::table('tutorials', function (Blueprint $table) {
-            $table->dropColumn('views');
+            $table->integer('views');
         });
     }
 
@@ -26,7 +26,7 @@ class DropViewsToTutorials extends Migration
     public function down()
     {
         Schema::table('tutorials', function (Blueprint $table) {
-            //
+            $table->integer('views');
         });
     }
 }
