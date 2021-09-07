418fedd491e424fa12149ae2c281af7a8aec685c
adamray <adamrayw@users.noreply.github.com>
2021-09-07 21:33:28 +0700

    Add some hanling logic for manage comments, courses, users

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\Course;
 use App\Models\Tutorials;
 use App\Models\User;
@@ -19,6 +20,43 @@ class AdminController extends Controller
         $tutorials = Tutorials::all();
         // get courses
         $courses = Course::all();
-        return view('admin/admin-dashboard', compact(['users', 'tutorials', 'courses']));
+        // get comments
+        $comments = Comment::orderBy('created_at', 'desc')->with(['user', 'tutorial'])->get();
+
+
+        return view(
+            'admin/admin-dashboard',
+            compact(
+                [
+                    'users',
+                    'tutorials',
+                    'courses',
+                    'comments'
+                ]
+            )
+        );
+    }
+
+    public function manageUsers()
+    {
+        // get users
+        $users = User::orderBy('created_at', 'desc')->get();
+
+        return view('admin.pages.manage-users', compact(['users']));
+    }
+
+    public function manageCourses()
+    {
+        // get users
+        $courses = Course::orderBy('created_at', 'desc')->with('category')->get();
+
+        return view('admin.pages.manage-courses', compact(['courses']));
+    }
+
+    public function manageComments()
+    {
+        $comments = Comment::orderBy('created_at', 'desc')->with(['tutorial'])->get();
+
+        return view('admin.pages.manage-comments', compact('comments'));
     }
 }
