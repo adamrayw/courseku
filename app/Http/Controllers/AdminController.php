9b6ab6a02c705c30485928cc5f9534d8db92b815
adamray <adamrayw@users.noreply.github.com>
2021-09-08 01:06:33 +0700

    Add logic to handling delete course

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Course;
 use App\Models\Tutorials;
 use App\Models\User;
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -58,5 +58,33 @@ class AdminController extends Controller
         $comments = Comment::orderBy('created_at', 'desc')->with(['tutorial'])->get();
 
         return view('admin.pages.manage-comments', compact('comments'));
+    }
+
+    public function addCourse()
+    {
+        // get category
+        $categories = Category::all();
+        return view('admin.pages.add-course', compact('categories'));
+    }
+
+    public function storeCourse(Request $request)
+    {
+        $validated = $request->validate([
+            'category_id' => 'required',
+            'name' => 'required',
+            'slug' => 'required',
+            'img_url' => 'required'
+        ]);
+
+        Course::create($validated);
+
+        return back()->with('success', 'Course added successfully!');
+    }
+
+    public function deleteCourse($id)
+    {
+        Course::where('id', $id)->delete();
+
+        return back()->with('successDelete', 'Course deleted successfully!');
     }
 }
