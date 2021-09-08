56c353873ea7153b530e71c9e447247284d74535
adamray <adamrayw@users.noreply.github.com>
2021-09-08 23:09:53 +0700

    Add route for delete user

--- routes/admin.php
+++ routes/admin.php
@@ -24,6 +24,8 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
     Route::get('/manage-users', [AdminController::class, 'manageUsers'])->middleware('auth:admin');
 
+    Route::get('/manage-users/{id}', [AdminController::class, 'deleteUser'])->middleware('auth:admin');
+
     Route::get('/manage-courses', [AdminController::class, 'manageCourses'])->middleware('auth:admin');
 
     Route::get('/manage-comments', [AdminController::class, 'manageComments'])->middleware('auth:admin');
