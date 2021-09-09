57a660a0e51bc8fadb93831e6e8f5db2daf65b8f
adamray <adamrayw@users.noreply.github.com>
2021-09-09 11:39:56 +0700

    Add routes for add category

--- routes/admin.php
+++ routes/admin.php
@@ -36,6 +36,11 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
     Route::post('/add-course/post', [AdminController::class, 'storeCourse'])->middleware('auth:admin');
 
+    Route::get('/add-category', [AdminController::class, 'addCategory'])->middleware('auth:admin');
+
+    Route::post('/add-category/post', [AdminController::class, 'storeCategory'])->middleware('auth:admin');
+
+
     Route::get('/manage-courses/{id}', [AdminController::class, 'deleteCourse'])->middleware('auth:admin');
 
     Route::post('/manage-courses', [AdminController::class, 'updateCategory'])->middleware('auth:admin');
