f03f8c2077d2077a872f23a857c115d1a78288aa
adamray <adamrayw@users.noreply.github.com>
2021-09-09 11:38:44 +0700

    Update UI

--- resources/views/pages/view-tutorial.blade.php
+++ resources/views/pages/view-tutorial.blade.php
@@ -57,7 +57,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-h-12 text-yellow-700 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                                 </svg>
-                                <p class="text-yellow-700 text-left ml-2">You can't comment, because your account is being suspended!</p>
+                                <p class="text-yellow-700 text-left ml-2">You can't comment, your account has been suspended!</p>
                             </div>
                             @else
                             <form action="/course/{{$data->slug}}" method="POST">
