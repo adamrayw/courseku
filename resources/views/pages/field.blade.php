d04da87d4adda490d5c4b46c8140908e6c381447
adamray <adamrayw@users.noreply.github.com>
2021-09-10 00:16:41 +0700

    Improve field/category UI

--- resources/views/pages/field.blade.php
+++ resources/views/pages/field.blade.php
@@ -1,26 +1,29 @@
 @extends('layouts.app')
-
-@section('title', $title)
+@foreach ($courses as $c)
+@section('title', $c->name)
 @section('content')
 
 <section class="mt-20 text-center max-w-5xl mx-auto px-4">
     <div>
-        <h1 class="font-bold mb-1 text-gray-600 text-4xl">{{ $name_category }}</h1>
+        <h1 class="font-bold mb-1 text-gray-600 text-4xl">{{ $c->name }}</h1>
     </div>
     <div class="what-you-learn mb-20">
         <div class="grid grid-cols-1 md:grid-cols-4 gap-4 py-6">
-            @foreach ($courses as $course)
-            <a href="/learn/{{ $course->slug }}">
+
+            @foreach ($c->course as $f)
+            <a href="/learn/{{ $f->slug }}">
                 <div class="card flex items-center shadow-sm p-4 bg-white border border-gray-200 rounded-lg hover:shadow-lg transition">
-                    {!! $course->img_url !!}
-                    <p class="ml-2 text-gray-600">{{ $course->name }}</p>
+                    {!!$f->img_url!!}
+                    <p class="ml-2 text-gray-600">{{$f->name}}</p>
                 </div>
             </a>
             @endforeach
+
         </div>
         <div>
-            @if (count($courses) < 1) <p class="text-center my-2">Sorry, No Course Found</p> @endif
+            @if (count($c->course) < 1) <p class="text-center my-2">Sorry, No Course Found</p> @endif
         </div>
     </div>
 </section>
 @endsection
+@endforeach
