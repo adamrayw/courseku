99bad1d53ccbb4854834388731f1ece804733ac4
adamray <adamrayw@users.noreply.github.com>
2021-09-09 18:39:49 +0700

    Update color suspended alert

--- resources/views/pages/view-tutorial.blade.php
+++ resources/views/pages/view-tutorial.blade.php
@@ -53,8 +53,8 @@
                         <div class="overflow-hidden">
                             @if (Auth::check())
                             @if (Auth()->user()->status == 'Suspended')
-                            <div class="text-center flex items-center bg-yellow-100 border border-yellow-700 p-4 rounded-md">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-h-12 text-yellow-700 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <div class="text-center flex items-center bg-yellow-100 border border-yellow-600 p-4 rounded-md">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-h-12 text-yellow-600 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                                 </svg>
                                 <p class="text-yellow-700 text-left ml-2">You can't comment, your account has been suspended!</p>
