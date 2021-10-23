76c2c7c8b76bcb3bab03c6b875f567d42dd829ac
adamray <adamrayw@users.noreply.github.com>
2021-10-24 00:42:11 +0700

    Test API Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/programming', function() {
+    return response()->json([
+        'name' => 'Adam',
+        'age' => 19
+    ]);
+});
