b2519451d966b11ef53bc985d4814cf19bfb5d89
adamray <adamrayw@users.noreply.github.com>
2021-10-25 19:19:52 +0700

    Add image tag

--- resources/views/admin/pages/manage-courses.blade.php
+++ resources/views/admin/pages/manage-courses.blade.php
@@ -46,7 +46,7 @@
                             <td class="px-4 py-3 border">
                                 <div class="flex items-center text-sm">
                                     <div class="relative w-8 h-8 mr-3 rounded-full md:block">
-                                        {!! $course->img_url !!}
+                                        <img src=" {{  $course->img_url  }}" alt="logo_courses">
                                         <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
                                     </div>
                                     <div>
