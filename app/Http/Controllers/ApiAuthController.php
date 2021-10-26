e84468da63df11eb6e934e1fd1921df3b63aefed
adamray <adamrayw@users.noreply.github.com>
2021-10-26 21:04:54 +0700

    Add register method for Api Register

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class ApiAuthController extends Controller
 {
     public function login(Request $request) {
-        $fields = $$request->validate([
+        $fields = $request->validate([
             'email' => 'required|string',
             'password' => 'required|string'
         ]);
@@ -23,6 +24,32 @@ class ApiAuthController extends Controller
                 'message' => 'Bad Credentials'
             ], 401);
         }
+
+        // Token
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        // Save user data
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
 
         // Token
         $token = $user->createToken('myapptoken')->plainTextToken;
