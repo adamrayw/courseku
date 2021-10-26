e8981dd2b799e6eac49ed8b1bb8777e9234b0cc8
adamray <adamrayw@users.noreply.github.com>
2021-10-26 19:38:34 +0700

    Add method login for Api Login

---
+++ app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ApiAuthController extends Controller
+{
+    public function login(Request $request) {
+        $fields = $$request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // check password
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad Credentials'
+            ], 401);
+        }
+
+        // Token
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 200);
+    }
+}
