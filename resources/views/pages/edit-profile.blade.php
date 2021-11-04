6f899ac5acf498f3f570669e389ed1d46d48eb4a
adamray <adamrayw@users.noreply.github.com>
2021-11-04 20:26:06 +0700

    Change form label

--- resources/views/pages/edit-profile.blade.php
+++ resources/views/pages/edit-profile.blade.php
@@ -8,7 +8,7 @@
     <div class="bg-white shadow rounded-md">
         <div class="flex items-center p-4 text-gray-600">
             <i class="fas fa-edit mr-1 fa-lg"></i>
-            <span class="font-bold block text-2xl ml-2">Edit Profile</span>
+            <span class="font-bold block text-2xl ml-2">Edit Profil</span>
         </div>
         <hr>
         <!-- Body 🍺 -->
@@ -18,7 +18,7 @@
                 <input type="hidden" name="id" value="{{ Auth()->user()->id }}">
                 @foreach ($users as $user)
                 <div class="mb-4">
-                    <h2 class="text-gray-600 font-semibold mb-1">Full Name</h2>
+                    <h2 class="text-gray-600 font-semibold mb-1">Nama Lengkap</h2>
                     <input type="text" name="name" class="h-12 border w-full font-xs text-gray-500 @error('name') border-red-300 @enderror border-gray-300 p-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" placeholder="Full Name" value="{{ $user->name }}">
                     @error('name')
                     <div class="text-red-500 text-sm">{{ $message }}</div>
