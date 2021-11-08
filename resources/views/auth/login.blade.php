d62a938294e89cd00a790064a9c89612dbb72dca
adamray <adamrayw@users.noreply.github.com>
2021-11-08 11:59:24 +0700

    Change to bahasa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,9 +5,9 @@
 @section('content')
 
 <div class="my-20 mx-2 flex flex-col justify-center items-center">
-    <div class="bg-white w-full md:w-1/4 shadow rounded-md p-5 mx-2">
-        <h1 class="text-4xl font-bold mb-2 text-gray-600">Welcome Back!</h1>
-        <p class="text-sm text-gray-500">Want to learn something new?</p>
+    <div class="bg-white w-full md:w-1/4 shadow rounded-md p-6 mx-2">
+        <h1 class="text-4xl font-bold mb-2 text-gray-600">Selamat Datang Kembali!</h1>
+        <p class="text-sm text-gray-500">Ingin belajar hal baru?</p>
         <hr class="mt-2 mb-2">
         @if (session('status'))
         <div class=" alert alert-success" role="alert">
@@ -16,22 +16,21 @@
         @endif
         <form action="{{ route('login') }}" method="POST" class="mt-2">
             @csrf
-            <input type="email" name="email" class="h-12 border w-full font-xs text-gray-500 app @error('email') border-red-300 @enderror border-gray-300 p-2 mt-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" placeholder="Email" value="{{ old('email')}}" autofocus />
+            <input type="email" name="email" class="h-12 border w-full font-xs text-gray-500 app @error('email') border-red-300 @enderror border-gray-300 p-2 mt-4 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" placeholder="Email" value="{{ old('email')}}" autofocus />
             @error('email')
             <p class="text-red-600 text-sm font-light">{{ $message }}</p>
             @enderror
 
-            <input type="password" name="password" class="h-12 border w-full font-xs text-gray-500 app @error('password') border-red-300 @enderror border-gray-300 p-2 mt-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" id="pass" placeholder="Password" />
+            <input type="password" name="password" class="h-12 border w-full font-xs text-gray-500 app @error('password') border-red-300 @enderror border-gray-300 p-2 mt-4 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" id="pass" placeholder="Password" />
             @error('password')
             <p class="text-red-600  text-sm font-light">{{ $message }}</p>
             @enderror
 
-
             <div class="mt-6">
-                <a href="/forgot-password" class="font-medium text-sm text-gray-500 float-right">Forgot Password?</a>
+                <a href="/forgot-password" class="font-medium text-sm text-gray-500 float-right">Lupa Password?</a>
             </div>
             <button type="submit" class="mt-2 text-center w-full bg-blue-500 rounded-md text-white py-3 font-medium hover:bg-blue-700 transition">Login</button>
-            <p class="text-sm text-center my-4 text-gray-500">Don't have an account ? <a href="/register" class="underline">Sign Up</a></p>
+            <p class="text-sm text-center mt-6 text-gray-500">Belum memiliki akun? <a href="/register" class="underline">Sign Up</a></p>
         </form>
     </div>
 </div>
