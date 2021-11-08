6c9c2786940bbc116d643bf0101e231d622571f9
adamray <adamrayw@users.noreply.github.com>
2021-11-08 12:02:30 +0700

    Change button background color to blue

--- resources/views/pages/tutorials.blade.php
+++ resources/views/pages/tutorials.blade.php
@@ -35,7 +35,7 @@
         <div class="p-6 rounded-md shadow-md md:w-1/3 w-full mx-auto">
             <h5 class="text-xl font-semibold text-center text-gray-600">Tutorials not Found</h5>
             <p class="text-center text-gray-500 text-sm">Help us find tutorials for {{ $slug }}</p>
-            <a href="/submit-tutorial" type="submit" class="mt-4 bg-gray-800 text-white px-6 py-2 text-sm rounded-md hover:bg-gray-700 transition">Submit tutorial</a>
+            <a href="/submit-tutorial" type="submit" class="mt-4 bg-blue-500 text-white px-6 py-2 text-sm rounded-md hover:bg-blue-800 transition">Submit tutorial</a>
         </div>
         @endif
     </div>
