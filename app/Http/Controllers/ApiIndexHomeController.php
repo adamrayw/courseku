65a095d7144efd06e25df5fc53f3c2eb950665c7
adamray <adamrayw@users.noreply.github.com>
2021-11-09 21:08:55 +0700

    Add show method for show course based slug

--- app/Http/Controllers/ApiIndexHomeController.php
+++ app/Http/Controllers/ApiIndexHomeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Category;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Tutorials;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,5 +27,17 @@ class ApiIndexHomeController extends Controller
         $field = Category::all();
 
         return response()->json($field, Response::HTTP_OK);
+    }
+
+    public function show(Course $course, Tutorials $tutorials)
+    {
+        $tutorial = Tutorials::Where('course_id', $course->id)->where('status', 'Release')->with('votes')->get();
+        $data = [
+            'course_name' => $course->name,
+            'slug' => $course->slug,
+            'tutorial' => $tutorial,
+        ];
+
+        return response()->json($data, response::HTTP_OK);
     }
 }
