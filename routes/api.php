3b36917e4602003d98b523a00b75969a5e32292d
adamray <adamrayw@users.noreply.github.com>
2021-11-22 20:37:18 +0700

    Change endpoint for carousel

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::post('/register', [ApiAuthController::class, 'register']);
 
 Route::get('/home', [ApiIndexHomeController::class, 'index']);
 Route::get('/field', [ApiIndexHomeController::class, 'fields']);
-Route::get('/carousel', [ApiIndexHomeController::class, 'carousel_artikel']);
+Route::get('/carousel-artikel', [ApiIndexHomeController::class, 'carousel_artikel']);
 
 Route::get('/learn/{course:slug}', [ApiIndexHomeController::class, 'show']);
 Route::get('/course/{slug}', [ApiIndexHomeController::class, 'course']);
