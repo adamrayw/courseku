251f668a5134a5a3cc288de3a8567a5f77f2f4fd
Adam Ray <raywibowo68@gmail.com>
2021-11-30 16:41:02 +0700

    Add api routes for submit and get all language

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,7 @@ Route::post('/register', [ApiAuthController::class, 'register']);
 Route::get('/home', [ApiIndexHomeController::class, 'index']);
 Route::get('/field', [ApiIndexHomeController::class, 'fields']);
 Route::get('/carousel', [ApiIndexHomeController::class, 'carousel_artikel']);
+Route::get('/language', [ApiIndexHomeController::class, 'language']);
 
 Route::get('/learn/{course:slug}', [ApiIndexHomeController::class, 'show']);
 Route::get('/course/{slug}', [ApiIndexHomeController::class, 'course']);
@@ -44,3 +45,4 @@ Route::delete('/{tid}/{uid}', [ApiIndexHomeController::class, 'removevote']);
 Route::post('/storesave', [ApiIndexHomeController::class, 'storesave']);
 Route::delete('/save/{tid}/{uid}', [ApiIndexHomeController::class, 'removesave']);
 
+Route::post('/submit', [ApiIndexHomeController::class, 'submit']);
