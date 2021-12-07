6ca5f7ba2bd3cec0fd339049aeb703f907093432
Adam Ray <raywibowo68@gmail.com>
2021-12-07 19:35:08 +0700

    Redesign home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,25 +3,34 @@
 @section('title', 'Tempat terbaik untuk mencari kursus & tutorial pemrograman')
 
 @section('content')
-    <section class="max-w-7xl mx-auto px-4">
-        <div class="flex justify-center text-center items-center mt-20 mb-28">
+    <section>
+        <div
+            class="max-w-7xl mx-auto flex md:flex-row flex-col text-center md:text-left justify-between items-center pt-20 md:pb-28 pb-0 ">
             <div class="px-4">
-                <h1 class="animate__animated animate__fadeInUp font-bold mb-2 text-gray-600 text-4xl">Selamat datang di
+                <h1 class="font-bold mb-2 text-gray-600 text-4xl">Selamat datang di
                     Courseku!</h1>
-                <p class="animate__animated animate__fadeInUp animate__delay-1s text-gray-400 text-lg">Tempat terbaik untuk
+                <p class="animate__delay-1s text-gray-500 text-lg">Tempat terbaik untuk
                     mencari course pemrograman & design</p>
                 <a href="/explore"
-                    class="animate__animated animate__fadeInUp animate__delay-2s inline-block font-semibold mt-6 rounded-lg no-underline text-white bg-blue-500 hover:bg-blue-700 transition-all px-6 py-3">Explore
+                    class="animate__delay-2s inline-block font-semibold mt-6 rounded-lg no-underline text-white bg-blue-500 hover:bg-blue-700 transition-all px-6 py-3">Explore
                     Tutorial</a>
             </div>
+            <div class="md:visible invisible">
+                <img src="/img/hero.svg" alt="hero" width="260">
+            </div>
         </div>
-        <div class="flex justify-between items-center mt-10 px-4 md:px-0" id="find-course">
+        <div class="max-w-7xl mx-auto">
+            <hr>
+        </div>
+    </section>
+    <section class="max-w-7xl mx-auto md:px-0 px-4">
+        <div class="flex justify-between items-center mt-20 px-4 md:px-0" id="find-course">
             <h1 class="text-2xl md:text-2xl font-semibold text-gray-600">Apa yang ingin kamu pelajari? </h1>
             <a href="/programming" class="md:visible invisible text-blue-500 hover:text-blue-700 transition">Lihat lebih
                 banyak <i class="fas fa-chevron-right fa-lg ml-1"></i></a>
         </div>
         <div class="what-you-learn mb-20">
-            <div class="grid grid-cols-1 md:grid-cols-5 md:px-0 px-4 gap-4 py-6" >
+            <div class="grid grid-cols-1 md:grid-cols-5 md:px-0 px-4 gap-4 py-6">
                 @foreach ($courses as $course)
                     <a href="/learn/{{ $course->slug }}">
                         <div
@@ -38,7 +47,7 @@
                     <i class="fas fa-chevron-right fa-lg ml-1"></i></a>
             </div>
         </div>
-        <div class="mt-10 px-4 md:px-0" >
+        <div class="mt-10 px-4 md:px-0">
             <h1 class="text-2xl md:text-2xl font-semibold text-gray-600">Cari sesuai bidang kamu</h1>
         </div>
         <div class="what-you-learn mb-20">
@@ -56,12 +65,17 @@
         </div>
     </section>
     <div class="app-banner bg-blue-500" id="banner">
-        <div class="desc-banner flex flex-col md:flex-row justify-around items-center max-w-7xl mx-auto left-0 top-0 py-8 px-5 text-white">
-            <div class="mr-4 text-center md:text-left mb-10 md:mb-0" >
-                <h1 class="text-2xl md:text-5xl font-bold" data-aos="fade-up" data-aos-duration="1000">Belajar di mana aja <br>dalam genggaman.</h1>
-                <p class="text-xs mt-4 md:text-xl font-medium" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">Sekarang Courseku punya aplikasi nya.</p>
+        <div
+            class="desc-banner flex flex-col md:flex-row justify-around items-center max-w-7xl mx-auto left-0 top-0 py-8 px-5 text-white">
+            <div class="mr-4 text-center md:text-left mb-10 md:mb-0">
+                <h1 class="text-2xl md:text-5xl font-bold" data-aos="fade-up" data-aos-duration="1000">Belajar di mana aja
+                    <br>dalam genggaman.
+                </h1>
+                <p class="text-xs mt-4 md:text-xl font-medium" data-aos="fade-up" data-aos-duration="1000"
+                    data-aos-delay="300">Sekarang Courseku punya aplikasi nya.</p>
                 <a href="https://dl.dropbox.com/s/a8w6nwbrpt00701/app.apk?dl=0"
-                    class="inline-block text-center mt-4 text-sm bg-white py-2 px-6 rounded-md text-blue-500 hover:bg-blue-700 transition" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
+                    class="inline-block text-center mt-4 text-sm bg-white py-2 px-6 rounded-md text-blue-500 hover:bg-blue-700 transition"
+                    data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                     <i class="fab fa-android fa-lg"></i>
                     <p>Android</p>
                 </a>
@@ -70,7 +84,8 @@
                     <i class="fab fa-apple fa-lg"></i>
                     <p>IOS</p>
                 </a> --}}
-                <p class="text-gray-400 mt-2 text-xs" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">Version 2.0.0</p>
+                <p class="text-gray-400 mt-2 text-xs" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
+                    Version 2.0.0</p>
             </div>
             <div class="mockup" data-aos="flip-left" data-aos-duration="1600">
                 <img src="/img/mockup.png" alt="mockup" class="h-auto w-64">
