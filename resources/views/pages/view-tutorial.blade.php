25e4e2926bedf785f4dfbbc05beb66dd78ca16fb
unknown <raywibowo68@gmail.com>
2021-12-09 21:18:34 +0700

    Redesign and change the layout a bit

--- resources/views/pages/view-tutorial.blade.php
+++ resources/views/pages/view-tutorial.blade.php
@@ -5,15 +5,15 @@
 @section('content')
 
     @foreach ($datas as $data)
-        <section class="max-w-7xl mt-20 md:mx-auto mx-4 shadow-lg rounded-b-lg">
+        <section class="max-w-5xl mt-20 md:mx-auto mx-4 shadow-lg rounded-b-lg">
             <div class="relative text-left bg-blue-500 md:h-36 h-40 rounded-t-lg" id="particles-js">
                 <div class="absolute w-full py-8 md:px-12 px-4">
                     <h1 class="text-white font-semibold md:text-2xl text-xl">{{ $data->name }}</h1>
                     <div class="flex justify-between items-center mt-6">
                         <div class="flex items-center">
-                            <p class=" bg-white px-2 py-1 rounded-md md:text-sm text-xs text-blue-500 mr-2">
+                            <p class="shadow-lg bg-white px-2 py-1 rounded-md md:text-sm text-xs text-blue-500 mr-2">
                                 {{ $data->type }} </p>
-                            <p class=" bg-white px-2 py-1 rounded-md md:text-sm text-xs text-blue-500">
+                            <p class="shadow-lg bg-white px-2 py-1 rounded-md md:text-sm text-xs text-blue-500">
                                 {{ $data->level }} </p>
                         </div>
                         <div>
@@ -43,11 +43,24 @@
                     <h2 class="text-gray-600 font-semibold md:text-2xl text-base mb-1">Description</h2>
                     <p class="text-gray-500 md:text-sm text-xs">{{ $data->description }}</p>
                 </div>
-                <div class="mt-6 btn-mulai-belajar text-right">
-                    <a class="md:inline-block block bg-blue-600 px-6 md:py-4 py-2 rounded-md md:text-xl text-base text-center font-semibold text-white hover:bg-blue-700 transition"
-                    href="{{ $data->source_link }}">Mulai Belajar</a>
+                <div class="mt-6 btn-mulai-belajar">
+                    <a class="md:inline-block block bg-blue-600 px-6 md:py-2 py-2 rounded-md md:text-lg text-base text-center font-semibold text-white hover:bg-blue-700 transition"
+                        href="{{ $data->source_link }}">Mulai Belajar</a>
                 </div>
                 <hr class="mt-4">
+                <div class="mt-6">
+                    @auth
+                        <form method="POST">
+                            @csrf
+                            <input type="hidden" name="users_id" value="{{ Auth::user()->id }}">
+                            <input type="hidden" name="tutorials_id" value="{{ $data->id }}">
+                            <textarea type="text"
+                                class="w-full md:inline-block block border border-transparent px-4 py-2 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition"
+                                name="comment" placeholder="Ketik Komentar..." required></textarea>
+                            <button type="submit" class="px-4 py-2 bg-blue-500 active:bg-blue-700 shadow-lg transition text-white rounded-md">POST</button>
+                        </form>
+                    @endauth
+                </div>
             </main>
         </section>
     @endforeach
