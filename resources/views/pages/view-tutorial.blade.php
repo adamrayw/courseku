26b5b292c7d6d6a75d4478e5725b15b3be6c198b
unknown <raywibowo68@gmail.com>
2021-12-10 23:00:08 +0700

    Redesign comment section

--- resources/views/pages/view-tutorial.blade.php
+++ resources/views/pages/view-tutorial.blade.php
@@ -40,86 +40,94 @@
                 </div>
                 <hr class="mt-4">
                 <div class="mt-6">
-                    @if (Auth::user())
 
-                        {{-- <form method="POST">
-                            @csrf
-                            <input type="hidden" name="users_id" value="{{ Auth::user()->id }}">
-                            <input type="hidden" name="tutorials_id" value="{{ $data->id }}">
+                    <div x-data="{ showModal : false }">
+                        <div class="flex justify-end items-center">
                             <div>
-                                <textarea type="text"
-                                    class="w-full mb-2 md:inline-block block border border-transparent px-4 py-2 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition"
-                                    name="comment" placeholder="Ketik Komentar..." required></textarea>
+                                @if (Auth::check())
+                                    @livewire('like-save', ['datas' => $datas, 'users_id' => Auth()->user()->id,
+                                    'tutorials_id' => $data->id, 'vote' => 1])
+                                @else
+                                    @livewire('like-save', ['datas' => $datas, 'users_id' => 0, 'tutorials_id' =>
+                                    $data->id,
+                                    'vote' => 1])
+                                @endif
                             </div>
-                            <div class="text-right">
-                                <button type="submit"
-                                    class="px-4 py-2 bg-blue-500 active:bg-blue-700 shadow-lg transition text-white rounded-md">POST</button>
-                            </div>
-                        </form> --}}
-                        <div x-data="{ showModal : false }">
-                            <div class="flex justify-end items-center">
-                                <div>
-                                    @if (Auth::check())
-                                        @livewire('like-save', ['datas' => $datas, 'users_id' => Auth()->user()->id,
-                                        'tutorials_id' => $data->id, 'vote' => 1])
-                                    @else
-                                        @livewire('like-save', ['datas' => $datas, 'users_id' => 0, 'tutorials_id' =>
-                                        $data->id,
-                                        'vote' => 1])
-                                    @endif
-                                </div>
-                                <button @click="showModal = !showModal" class="text-lg  ml-6 text-gray-600"><i
-                                        class="far fa-comment fa-lg mr-1"></i> {{ count($data->comments) }}</button>
-                            </div>
+                            <button @click="showModal = !showModal" class="text-lg  ml-6 text-gray-600"><i
+                                    class="far fa-comment fa-lg mr-1"></i> {{ count($data->comments) }}</button>
+                        </div>
 
-                            <div x-show=" showModal"
-                                class="fixed text-gray-500 flex items-center justify-center overflow-auto z-50 bg-black bg-opacity-40 left-0 right-0 top-0 bottom-0"
-                                x-transition:enter="transition ease duration-300" x-transition:enter-start="opacity-0"
-                                x-transition:enter-end="opacity-100" x-transition:leave="transition ease duration-300"
-                                x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
-                                <!-- Modal -->
-                                <div x-show="showModal" class="bg-white h-60 rounded-xl shadow-2xl p-6 w-full sm:w-5/12 mx-10"
-                                    @click.away="showModal = false"
-                                    x-transition:enter="transition ease duration-100 transform"
-                                    x-transition:enter-start="opacity-0 scale-90 translate-y-1"
-                                    x-transition:enter-end="opacity-100 scale-100 translate-y-0"
-                                    x-transition:leave="transition ease duration-100 transform"
-                                    x-transition:leave-start="opacity-100 scale-100 translate-y-0"
-                                    x-transition:leave-end="opacity-0 scale-90 translate-y-1">
-                                    <div class="mb-1 flex justify-between items-center">
-                                        <h1 class=" text-xl">Comments
-                                        </h1>
-                                        <div class="text-center">
-                                            <button @click="showModal = !showModal" class=""><i
-                                                    class="fas fa-times"></i></button>
-                                        </div>
+                        <div x-show=" showModal"
+                            class="fixed text-gray-500 flex items-center justify-center overflow-auto z-50 bg-black bg-opacity-40 left-0 right-0 top-0 bottom-0"
+                            x-transition:enter="transition ease duration-300" x-transition:enter-start="opacity-0"
+                            x-transition:enter-end="opacity-100" x-transition:leave="transition ease duration-300"
+                            x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
+                            <!-- Modal -->
+                            <div x-show="showModal" class="bg-white rounded-xl shadow-2xl p-6 w-full sm:w-5/12 mx-14"
+                                @click.away="showModal = false" x-transition:enter="transition ease duration-100 transform"
+                                x-transition:enter-start="opacity-0 scale-90 translate-y-1"
+                                x-transition:enter-end="opacity-100 scale-100 translate-y-0"
+                                x-transition:leave="transition ease duration-100 transform"
+                                x-transition:leave-start="opacity-100 scale-100 translate-y-0"
+                                x-transition:leave-end="opacity-0 scale-90 translate-y-1">
+                                <div class="mb-1 flex justify-between items-center">
+                                    <h1 class=" text-xl">Comments
+                                    </h1>
+                                    <div class="text-center">
+                                        <button @click="showModal = !showModal" class=""><i
+                                                class="fas fa-times"></i></button>
                                     </div>
-                                    <hr>
-                                    <div class="mt-4">
-                                        @foreach ($data->comments as $comm)
-                                            <div class="flex justify-between items-center">
-                                                <div class="flex items-center">
-                                                    <div
-                                                        class="flex justify-center items-center h-10 py-2 px-4 mr-3 text-white bg-gray-600 rounded-full">
-                                                        <p>{{ $comm->user->name[0] }}</p>
-                                                    </div>
-                                                    <div>
-                                                        <p class="font-medium text-gray-600">{{ $comm->user->name }}</p>
-                                                        <p class="text-sm text-gray-500">{{ $comm->comment }}</p>
-                                                    </div>
+                                </div>
+                                <hr>
+                                <div class="mt-4 h-60 overflow-auto">
+                                    @foreach ($data->comments as $comm)
+                                        <div
+                                            class="flex md:flex-row flex-col justify-between md:items-center items-start my-4">
+                                            <div class="flex items-center ">
+                                                <div
+                                                    class="flex justify-center items-center md:h-10 h-8 py-2 md:px-4 px-3 mr-3 text-white bg-gray-600 rounded-full">
+                                                    <p class="md:text-sm text-xs">{{ $comm->user->name[0] }}</p>
                                                 </div>
-                                                <p class="text-gray-400 font-light">{{ $comm->created_at->diffForHumans() }}</p>
+                                                <div>
+                                                    <p class="font-medium md:text-base text-sm text-gray-600">
+                                                        {{ $comm->user->name }}</p>
+                                                    <p class="md:text-sm text-xs text-gray-500 break-words">
+                                                        {{ $comm->comment }}</p>
+                                                </div>
                                             </div>
-                                        @endforeach
-                                    </div>
+                                            <p
+                                                class="text-gray-400 md:mt-0 mt-2 ml-auto text-right md:text-sm text-xs font-light">
+                                                {{ $comm->created_at->diffForHumans() }}
+                                            </p>
+                                        </div>
+                                        <hr>
+                                    @endforeach
+                                </div>
+                                <div class="mt-6">
+                                    @if (Auth::user())
+                                        <form method="POST">
+                                            @csrf
+                                            <input type="hidden" name="users_id" value="{{ Auth::user()->id }}">
+                                            <input type="hidden" name="tutorials_id" value="{{ $data->id }}">
+                                            <div>
+                                                <textarea type="text"
+                                                    class="w-full mb-2 text-xs inline-block border border-transparent px-4 py-2 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition"
+                                                    name="comment" placeholder="Ketik Komentar..." required></textarea>
+                                            </div>
+                                            <div class="text-right ">
+                                                <button type="submit"
+                                                    class="px-4 py-2 md:textt-base text-sm bg-blue-500 active:bg-blue-700 shadow-lg transition text-white rounded-md">POST</button>
+                                            </div>
+                                        </form>
+                                    @else
+                                        <h2 class="text-blue-500 text-center">You must <a class="underline semibold"
+                                                href="/login">Login</a>
+                                            to comment!</h2>
+                                    @endif
                                 </div>
                             </div>
                         </div>
-                    @else
-                        <h2 class="text-blue-500 text-center">You must <a class="underline semibold" href="/login">Login</a>
-                            to comment!</h2>
-                    @endif
-
+                    </div>
                 </div>
             </main>
         </section>
