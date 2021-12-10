07aed0a67708eb1e51b7eb099f52b227ebf444ea
unknown <raywibowo68@gmail.com>
2021-12-10 23:03:15 +0700

    Add mx-4 on modal

--- resources/views/pages/view-tutorial.blade.php
+++ resources/views/pages/view-tutorial.blade.php
@@ -63,7 +63,7 @@
                             x-transition:enter-end="opacity-100" x-transition:leave="transition ease duration-300"
                             x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
                             <!-- Modal -->
-                            <div x-show="showModal" class="bg-white rounded-xl shadow-2xl p-6 w-full sm:w-5/12 mx-14"
+                            <div x-show="showModal" class="bg-white rounded-xl shadow-2xl p-6 w-full sm:w-5/12 mx-4"
                                 @click.away="showModal = false" x-transition:enter="transition ease duration-100 transform"
                                 x-transition:enter-start="opacity-0 scale-90 translate-y-1"
                                 x-transition:enter-end="opacity-100 scale-100 translate-y-0"
