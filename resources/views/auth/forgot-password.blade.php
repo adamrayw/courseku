eeacfe60d035336d9f6e29d1c0c16020c3a8076c
unknown <raywibowo68@gmail.com>
2021-12-11 12:33:16 +0700

    Redesign forgot password and reset password page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,10 +1,12 @@
-@extends('layouts.app', ['title' => 'Forgot Password - SantriKoding.com'])
+@extends('layouts.app')
+
+@section('title', 'Forgot Password')
 
 @section('content')
 <div class="my-20 mx-2 bg-white flex flex-col justify-center items-center">
     <div class="bg-white w-full md:w-1/4 shadow rounded-md p-5 mx-2">
         <h1 class="text-4xl font-bold mb-2 text-gray-600">Forgot Password</h1>
-        <p class="text-sm text-gray-500">Just enter your registered email, we will email you a link to reset your password.</p>
+        <p class="text-sm text-gray-500">Cukup masukkan email terdaftar Anda, kami akan mengirimkan email berisi tautan untuk mereset kata sandi Anda.</p>
         <hr class="mt-2 mb-2">
         <div class="card-body">
             @if (session('status'))
@@ -20,13 +22,13 @@
                     <input id="email" type="email" class="h-12 border w-full font-xs text-gray-500 app border-gray-300 p-2 my-2 rounded-md focus:outline-none focus:ring focus:ring-indigo-300" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder=" Email">
 
                     @error('email')
-                    <div class="alert alert-danger mt-2">
+                    <div class="alert text-red-500 mt-2">
                         <strong>{{ $message }}</strong>
                     </div>
                     @enderror
                 </div>
 
-                <button type="submit" class="mt-2 text-center text-base w-full bg-gray-700 rounded-md text-white py-3 font-medium hover:bg-gray-600 transition">SEND PASSWORD RESET LINK</button>
+                <button type="submit" class="mt-2 text-center text-base w-full bg-blue-500 hover:bg-blue-700 transition rounded-md text-white py-3 font-medium hover:bg-gray-600 transition">SEND PASSWORD RESET LINK</button>
             </form>
         </div>
     </div>
