9ea79da61a7bd29b1c6be7962ecd4092c07f634d
unknown <raywibowo68@gmail.com>
2021-12-11 19:51:33 +0700

    Top Contributor Page

---
+++ resources/views/pages/top-contributor.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'Top Contributor')
+
+@section('content')
+
+<section class="my-20 max-w-3xl mx-4 md:mx-auto">
+    <div class="bg-white shadow rounded-md">
+        <div class="flex items-center p-4 text-gray-600">
+            <i class="fas fa-medal mr-1 fa-lg"></i>
+            <span class="font-bold block text-2xl ml-2">Top Contributor</span>
+        </div>
+        <hr>
+        <!-- Body 🍺 -->
+        <div class="p-6 md:px-8 md:w-auto w-full">
+            <h1>Maaf, Halaman ini sedang dikerjakan...</h1>
+        </div>
+    </div>
+</section>
+@endsection
