6ed681cef09384b5549ace657f27a3b761f170c2
unknown <raywibowo68@gmail.com>
2021-12-13 21:19:56 +0700

    Now top contributor is working

--- resources/views/pages/top-contributor.blade.php
+++ resources/views/pages/top-contributor.blade.php
@@ -4,17 +4,33 @@
 
 @section('content')
 
-<section class="my-20 max-w-3xl mx-4 md:mx-auto">
-    <div class="bg-white shadow rounded-md">
-        <div class="flex items-center p-4 text-gray-600">
-            <i class="fas fa-medal mr-1 fa-lg"></i>
-            <span class="font-bold block text-2xl ml-2">Top Contributor</span>
+    <section class="my-20 max-w-3xl mx-4 md:mx-auto">
+        <div class="bg-white shadow rounded-md">
+            <div class="flex flex-col items-start p-4 text-gray-600">
+                <span class="font-bold block text-2xl ml-2"><i class="fas fa-medal mr-2 fa-md"></i>Top 5 Contributor</span>
+            </div>
+            <hr>
+            <!-- Body 🍺 -->
+            <div class="p-6 md:px-8 md:w-auto w-full">
+                <table class="w-full">
+                    <thead>
+                        <tr class="font-semibold">
+                            <td>No</td>
+                            <td>Nama</td>
+                            <td>Points</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr class="text-gray-500">
+                                <td>{{ $loop->iteration }}.</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->points }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <hr>
-        <!-- Body 🍺 -->
-        <div class="p-6 md:px-8 md:w-auto w-full">
-            <h1>Maaf, Halaman ini sedang dikerjakan...</h1>
-        </div>
-    </div>
-</section>
+    </section>
 @endsection
