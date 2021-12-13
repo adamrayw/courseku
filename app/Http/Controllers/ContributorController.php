7f7684a8d66a7ce3932803a4e8ea1713a32e0109
unknown <raywibowo68@gmail.com>
2021-12-13 21:12:35 +0700

    Add logic for find user by most points

--- app/Http/Controllers/ContributorController.php
+++ app/Http/Controllers/ContributorController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ContributorController extends Controller
 {
-    public function contributor() {
-        return view('pages.top-contributor');
+    public function contributor()
+    {
+        $user = User::orderBy('points', 'desc')->get();
+        dd($user);
+
+        return view('pages.top-contributor', compact('user'));
     }
 }
