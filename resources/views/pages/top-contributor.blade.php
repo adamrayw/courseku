8f3567ac83f2670f5165c910400bd2d6775ddaaa
unknown <raywibowo68@gmail.com>
2021-12-13 22:18:23 +0700

    Add message if users null

--- resources/views/pages/top-contributor.blade.php
+++ resources/views/pages/top-contributor.blade.php
@@ -8,7 +8,7 @@
         <div class="bg-white shadow rounded-md">
             <div class="flex items-center p-4">
                 <i class="fas fa-medal text-orange-300 mr-2 fa-md fa-lg"></i>
-                <span class="font-bold block text-2xl ml-2 text-blue-600">
+                <span class="font-bold block text-2xl ml-2 text-blue-500">
                     Top 5
                     Contributor</span>
             </div>
@@ -17,11 +17,13 @@
             <div class="p-6 md:px-8 md:w-auto w-full">
                 <table class="w-full border-collapse">
                     <thead>
-                        <tr class="font-semibold text-left pb-1 text-blue-600">
-                            <th>No</th>
-                            <th>Nama</th>
-                            <th>Points</th>
-                        </tr>
+                        @if (count($users) > 0)
+                            <tr class="font-semibold text-left pb-1 text-blue-500">
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Points</th>
+                            </tr>
+                        @endif
                     </thead>
                     <tbody>
                         @foreach ($users as $user)
@@ -33,6 +35,14 @@
                         @endforeach
                     </tbody>
                 </table>
+                @if (count($users) == 0)
+                    <div class="text-center mb-6">
+                        <h5 class="pt-6 pb-2">
+                            let's submit a course, and be a top contributor.
+                        </h5>
+                        <a href="/submit-tutorial" class="underline">Submit Course</a>
+                    </div>
+                @endif
             </div>
         </div>
     </section>
