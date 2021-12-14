00501acc0c5ad7e2c834581be136192a8e80c0bf
unknown <raywibowo68@gmail.com>
2021-12-14 13:26:14 +0700

    Fix name_course

--- resources/views/pages/tutorials.blade.php
+++ resources/views/pages/tutorials.blade.php
@@ -5,9 +5,8 @@
 @section('content')
     <section class="mt-20 text-center max-w-5xl mx-auto px-4">
         <div class="text-left">
-            <h1 class="font-bold mb-1 text-gray-600 sm:text-3xl text-xl">Learn {{ $name_course }}</h1>
-            <p class="sm:text-lg text-sm text-gray-500">Mari belajar {{ strtolower($name_course) }}, course ini dikirim
-                oleh berbagai user.</p>
+            <h1 class="font-bold mb-1 text-gray-600 sm:text-3xl text-xl">Belajar {{ $name_course }}</h1>
+            <p class="sm:text-lg text-sm text-gray-500">Course ini dikirim oleh berbagai user.</p>
         </div>
         <div class="what-you-learn mb-20">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4 py-6">
@@ -53,7 +52,7 @@
                 <div class="p-6 rounded-md shadow-md md:w-1/3 w-full mx-auto">
                     <h5 class="text-xl font-semibold text-center text-gray-600">Course tidak tersedia</h5>
                     <p class="mt-1 text-center text-gray-500 text-xs">Bantu kami mencari course untuk
-                        {{ Str::lower($$name_course) }}
+                        {{ Str::lower($name_course) }}
                     </p>
                     <a href="/submit-tutorial" type="submit"
                         class="mt-6 bg-blue-500 text-white px-6 py-2 text-sm rounded-md hover:bg-blue-800 transition">Kirim
