91f1c5946baa8dae369e27767e4f3c0a376feffa
Adam Ray Wibowo <raywibowo68@gmail.com>
2021-12-20 20:49:58 +0700

    Add display:none for x-cloak

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,10 @@
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap');
 
+        [x-cloak] {
+            display: none !important;
+        }
+
         html {
             font-family: 'Poppins', sans-serif;
             scroll-behavior: smooth;
